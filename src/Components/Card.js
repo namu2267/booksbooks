cb31bc67c29fa9dca505e8ed0f6e01ab6874eaa1
namu2267 <namu2267@gmail.com>
2023-04-11 22:42:25 +0900

    BooksBooks기본기능완성

---
+++ src/Components/Card.js
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import Modal from "./Modal";
+
+export default function Card({ book }) {
+  console.log(book);
+
+  const [show, setShow] = useState(false);
+  const [bookItem, setItem] = useState();
+  return (
+    <>
+      {book.map((item) => {
+        let thumbnail =
+          item.volumeInfo.imageLinks &&
+          item.volumeInfo.imageLinks.smallThumbnail;
+        let amount = item.saleInfo.listPrice && item.saleInfo.listPrice.amount;
+        let title = item.volumeInfo.title;
+        if (thumbnail != undefined && amount != undefined) {
+          return (
+            <>
+              <div
+                className="card"
+                onClick={() => {
+                  setShow(true);
+                  setItem(item);
+                }}
+              >
+                <img src={thumbnail} />
+                <div className="bottom">
+                  <h3 className="title">{title}</h3>
+                  <p className="amount">&#8377; {amount} </p>
+                </div>
+              </div>
+            </>
+          );
+        }
+      })}
+      <Modal show={show} item={bookItem} onClose={() => setShow(false)} />
+    </>
+  );
+}
---
+++ src/Components/Main.js
@@ -0,0 +1,62 @@
+import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import Card from "./Card";
+import axios from "axios";
+
+export default function Main() {
+  const [search, setSearch] = useState("");
+  const [bookData, setData] = useState([]);
+  const API_KEY = process.env.REACT_APP_API_KEY;
+
+  const searchBook = () => {
+    axios
+      .get(
+        "https://www.googleapis.com/books/v1/volumes?q= " +
+          search +
+          `&key=${API_KEY}` +
+          "&maxResults=40"
+      )
+      .then((res) => setData(res.data.items))
+      .catch((err) => console.log(err.response.data));
+    console.log(API_KEY);
+  };
+
+  const handleSearch = (e) => {
+    if (e.key === "Enter") {
+      searchBook();
+    }
+  };
+
+  const handleButtonClick = () => {
+    searchBook();
+  };
+
+  return (
+    <>
+      <div className="header">
+        <div className="row1">
+          <h1>BooksBooks</h1>
+        </div>
+        <div className="row2">
+          <h2>책을 찾아보세요</h2>
+          <div className="search">
+            <input
+              type="text"
+              placeholder="책이름을 입력하세요"
+              value={search}
+              onChange={(e) => setSearch(e.target.value)}
+              onKeyPress={handleSearch}
+            />
+            <button onClick={handleButtonClick}>
+              <FontAwesomeIcon icon={faMagnifyingGlass} />
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className="container">
+        <Card book={bookData} />
+      </div>
+    </>
+  );
+}
